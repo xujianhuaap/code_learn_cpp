5744f5f86d3a9e56d670717be4d30d67202a0bb7
skullmind <xujianhuaap@gmail.com>
2020-11-02 23:11:10 +0800

    feature:init repo

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+void sayHi();
+int main() {
+    sayHi();
+    return 0;
+}
---
+++ src/linkage/ExternalLinkage.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+//
+// Created by xujianhua on 2020/11/1.
+//
+//全局常量默认是InternalLinkage
+ void sayHi(){
+    std::cout << "hi" << std::endl;
+}
